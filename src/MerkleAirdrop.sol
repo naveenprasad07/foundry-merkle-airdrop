4366c27c30ea663b361e64b9334bc5273a2ebd01
naveen@galaxy4 <naveenacad89@gmail.com>
2025-04-11 11:37:11 +0000

    initial-setup

---
+++ src/MerkleAirdrop.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract MerkleAirdrop{
+    //some list of addresses
+    // Allow someone in th list to claim ERC-20 tokens
+    address[] claimers;
+
+    
+}
